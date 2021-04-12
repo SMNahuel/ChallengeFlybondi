c40d49ddee10a19697a895c145b568d7eb37f3a5
Nahuelsan <nahuel.-sanchez@live.com.ar>
2021-04-12 18:48:26 -0300

    Update diagram

--- client/src/components/Chatbot/Chatbot.js
+++ client/src/components/Chatbot/Chatbot.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import ChatBot from 'react-simple-chatbot';
 const Chatbot = () => {
     return(
         <div>
-            
+            <ChatBot
+            steps={[
+                {
+                id: 'hello-world',
+                message: 'Hello World!',
+                end: true,
+                },
+            ]}
+            />
         </div>
     )
 }
